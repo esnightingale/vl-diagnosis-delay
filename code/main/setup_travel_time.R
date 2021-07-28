b7d008770802c7246bd64453853b225d2d66d404
Emily Nightingale <phpuenig@lshtm.ac.uk>
2021-07-28 18:33:35 +0100

    tidying

--- code/main/setup_travel_time.R
+++ code/main/setup_travel_time.R
@@ -10,17 +10,15 @@ library(abind)
 
 # ---------------------------------------------------------------------------- #
 
-# Get shapefile for Bihar in India
+# Get shapefile for Bihar in India - transform projection from lat/long
 
 ind.shp <- malariaAtlas::getShp(ISO = "IND", admin_level = "admin1")
-bh.shp <- ind.shp[ind.shp@data$name_1 == "Bihar",]
-
-sp::plot(bh.shp, main = "Bihar state boundary")
+bh.shp <- ind.shp[ind.shp@data$name_1 == "Bihar",] 
 
 # Retrieve friction surface from MAP
 friction <- malariaAtlas::getRaster(
   surface = "A global friction surface enumerating land-based travel speed for a nominal year 2015",
-  shp = bh.shp)
+  extent = bbox(bh.shp))
 
 malariaAtlas::autoplot_MAPraster(friction)
 
@@ -52,7 +50,7 @@ sp::coordinates(facility) <- ~ x + y
 sp::proj4string(facility) <- sp::proj4string(bh.shp)
 
 # Keep only point coordinates within the shapefile bounds (allow a small buffer)
-overlap <- sp::over(facility, raster::buffer(bh.shp, 0.05))
+overlap <- sp::over(facility, raster::buffer(bh.shp, 1))
 facility <- facility[!is.na(overlap),]
 
 sp::plot(bh.shp, main = "Health facilities with VL diagnosis/treatment capacity in Bihar")
@@ -79,6 +77,11 @@ points <- as.matrix(diag.facility@coords)
 # Calculate travel times via the "accumulated cost surface" algorithm
 access.raster <- gdistance::accCost(Tmat.GC, diag.facility)
 
+# Save raster of travel time to facility
+raster::writeRaster(access.raster, here::here("data","covariates","diag_facility_travel_time.tif"), overwrite = TRUE)
+
+# ---------------------------------------------------------------------------- #
+
 # Redefine infinite values as NA to avoid showing in plot
 access.raster@data@values[is.infinite(access.raster@data@values)] <- NA
 
@@ -104,118 +107,97 @@ full_plot <- full_plot +
            col = "white", cex = 0.8) 
 ggsave(here::here("figures","covariates","traveltime_wfacilities.png"), full_plot, height = 5, width = 7, units = "in")
 
-# Save raster of travel time to facility
-raster::writeRaster(access.raster, here::here("data","covariates","diag_facility_travel_time.tif"), overwrite = TRUE)
-
-# Convert to data.frame
-access.pts <- raster::rasterToPoints(access.raster, spatial = TRUE)
-access.df  <- data.frame(access.pts)
-
-saveRDS(access.df, here::here("data","covariates","diag_facility_travel_time.rds"))
-
-ggplot() +
-  geom_raster(data = access.df, aes(x = x, y = y, fill = diag_facility_travel_time)) +
-  scale_fill_viridis_c(trans = "sqrt") +
-  theme(axis.title = element_blank(),
-        axis.text = element_blank(),
-        panel.border = element_rect(fill = NA, colour = "white"),
-        panel.background = element_rect(fill = "white", colour = "white")) +
-  labs(x = "", y = "", 
-       title = "Travel time to most accessible health facility",
-       fill = "Minutes \nof travel")
-
-
 # ---------------------------------------------------------------------------- #
-
-villages <- filter(match, !is.na(longitude))
-coordinates(villages) <- ~ longitude + latitude
-
-vil.access <- data.frame(coordinates(villages),
-                         vil_code = villages$vil_code,
-                         sex = factor(villages$SEX),
-                         agecat = factor(villages$agecat),
-                         caste = factor(villages$caste4_r),
-                         hiv = factor(villages$HIV, levels = c(2,1), labels = c("Neg","Pos")),
-                         prev_trt_ka = factor(villages$Prv_TX_KA),
-                         acd = factor(villages$POSS_ACD),
-                         days_fever = villages$DUR_FEV_R,
-                         fever_gt30 = (villages$DUR_FEV_R > 30),
-                         fever_gt90 = (villages$DUR_FEV_R > 90),
-                         traveltime = raster::extract(access.raster, villages))
-
-ggplot(vil.access, aes(traveltime, days_fever)) +
-  geom_point(alpha = 0.5) +
-  geom_smooth() +
-  scale_x_continuous(trans = "log2") +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(fever_gt90, traveltime)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(sex, days_fever)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(agecat, days_fever)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(caste, days_fever)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(hiv, days_fever)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(prev_trt_ka, days_fever)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-ggplot(vil.access, aes(acd, days_fever)) +
-  geom_boxplot() +
-  scale_y_continuous(trans = "log2")
-
-
-# ---------------------------------------------------------------------------- #
-# Exploratory model
-
-dat <- vil.access %>%
-  dplyr::select(longitude, latitude, days_fever, fever_gt30, fever_gt30, sex, agecat, caste, hiv, prev_trt_ka, acd, traveltime) %>%
-  na.omit()
-
-coordinates(dat) <- ~ longitude + latitude
-
-mod <- glm(days_fever ~ sex + agecat + caste + hiv + prev_trt_ka + acd + traveltime, 
-           data = dat,
-           family = "poisson")
-
-summary(mod)
-plot(mod)
-
-# ---------------------------------------------------------------------------- #
-# Semi-variogram of model residuals
-
-dat$resid <- residuals(mod, "pearson")
-
-vg <- variogram(resid~1, data = dat)
-plot(vg)
-
-vgmod <- vgm(psill =  15, model = "Mat", nugget = 20, range = 0.5)
-plot(vg, model = vgmod)
-
-vgfit <- fit.variogram(vg, model = vgmod)    
-plot(vg, model = vgfit)
-
-vgfit
-# model    psill     range kappa
-# Nug 19.04896 0.0000000   0.0
-# Mat 21.70701 0.8020592   0.5
-
-png(here::here("figures","fit", "resid_variogram.png"), height = 600, width = 700, res = 150)
-plot(vg, model = vgfit)
-dev.off()
-
+# 
+# villages <- filter(match, !is.na(longitude))
+# coordinates(villages) <- ~ longitude + latitude
+# 
+# vil.access <- data.frame(coordinates(villages),
+#                          vil_code = villages$vil_code,
+#                          sex = factor(villages$SEX),
+#                          agecat = factor(villages$agecat),
+#                          caste = factor(villages$caste4_r),
+#                          hiv = factor(villages$HIV, levels = c(2,1), labels = c("Neg","Pos")),
+#                          prev_trt_ka = factor(villages$Prv_TX_KA),
+#                          acd = factor(villages$POSS_ACD),
+#                          days_fever = villages$DUR_FEV_R,
+#                          fever_gt30 = (villages$DUR_FEV_R > 30),
+#                          fever_gt90 = (villages$DUR_FEV_R > 90),
+#                          traveltime = raster::extract(access.raster, villages))
+# 
+# ggplot(vil.access, aes(traveltime, days_fever)) +
+#   geom_point(alpha = 0.5) +
+#   geom_smooth() +
+#   scale_x_continuous(trans = "log2") +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(fever_gt90, traveltime)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(sex, days_fever)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(agecat, days_fever)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(caste, days_fever)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(hiv, days_fever)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(prev_trt_ka, days_fever)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# ggplot(vil.access, aes(acd, days_fever)) +
+#   geom_boxplot() +
+#   scale_y_continuous(trans = "log2")
+# 
+# 
+# # ---------------------------------------------------------------------------- #
+# # Exploratory model
+# 
+# dat <- vil.access %>%
+#   dplyr::select(longitude, latitude, days_fever, fever_gt30, fever_gt30, sex, agecat, caste, hiv, prev_trt_ka, acd, traveltime) %>%
+#   na.omit()
+# 
+# coordinates(dat) <- ~ longitude + latitude
+# 
+# mod <- glm(days_fever ~ sex + agecat + caste + hiv + prev_trt_ka + acd + traveltime, 
+#            data = dat,
+#            family = "poisson")
+# 
+# summary(mod)
+# plot(mod)
+# 
+# # ---------------------------------------------------------------------------- #
+# # Semi-variogram of model residuals
+# 
+# dat$resid <- residuals(mod, "pearson")
+# 
+# vg <- variogram(resid~1, data = dat)
+# plot(vg)
+# 
+# vgmod <- vgm(psill =  15, model = "Mat", nugget = 20, range = 0.5)
+# plot(vg, model = vgmod)
+# 
+# vgfit <- fit.variogram(vg, model = vgmod)    
+# plot(vg, model = vgfit)
+# 
+# vgfit
+# # model    psill     range kappa
+# # Nug 19.04896 0.0000000   0.0
+# # Mat 21.70701 0.8020592   0.5
+# 
+# png(here::here("figures","fit", "resid_variogram.png"), height = 600, width = 700, res = 150)
+# plot(vg, model = vgfit)
+# dev.off()
+# 
 ################################################################################
 ################################################################################
